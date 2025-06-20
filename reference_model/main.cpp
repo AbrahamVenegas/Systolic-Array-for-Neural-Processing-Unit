e1c978c65e2a1f81e1332a0fbd691c115fc06ee7
Esteban <esteban25agosto@gmail.com>
2025-06-20 11:28:20 -0600

    Matrix GT4 Implementation

--- reference_model/main.cpp
+++ reference_model/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main () {
     // Crear una matriz de PEs
     const int size = 4;
+    
 
     // Arreglo de PEs 4x4
     vector<vector<PE>> pe_array(size, vector<PE>(size, PE(nullptr, nullptr)));
@@ -32,26 +33,35 @@ int main () {
 
     // Llenar la memoria con datos de ejemplo
     // Cargar matrices de ejemplo
-    vector<vector<int>> matrix_A = {{1, 2, 3, 4},
-                            {1, 6, 7, 8}, 
-                            {2, 1, 11, 12}, 
-                            {3, 14, 15, 16}};
+    int n = 8;
+    vector<vector<int>> matrix_A(n, vector<int>(n));
+    vector<vector<int>> matrix_B(n, vector<int>(n));
 
-    vector<vector<int>> matrix_B = {{1, 2, 3, 4},
-                            {1, 3, 5, 7}, 
-                            {2, 8, 9, 3}, 
-                            {3, 2, 7, 9}};
+    // Llenar matrix_A con valores de ejemplo (1, 2, ..., 256)
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            matrix_A[i][j] = i * n + j + 1;
+        }
+    }
 
-    
+    // Llenar matrix_B con valores de ejemplo (2, 4, ..., 512)
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            matrix_B[i][j] = 2 * (i * n + j + 1);
+        }
+    }
+
+    cout << "Cargando matrices A y B en la memoria..." << endl;
     // Guardar las matrices en la memoria
-    for (int i = 0; i < 16; ++i) {
-        memoryy.memory[i] = matrix_A[i / 4][i % 4]; // Guardar matriz A
+    for (int i = 0; i < n * n; ++i) {
+        memoryy.memory[i] = matrix_A[i / n][i % n]; // Guardar matriz A
     }
     
     int j;
-    for (int i = 0; i < 16; ++i) {
-        j = i + 16; // Offset para la matriz B
-        memoryy.memory[j] = matrix_B[i / 4][i % 4]; // Guardar matriz A
+    for (int i = 0; i < n * n; ++i) {
+        // Cuidado
+        j = i + (n * n); // Offset para la matriz B
+        memoryy.memory[j] = matrix_B[i / n][i % n]; // Guardar matriz B
     }
         
 
@@ -59,16 +69,17 @@ int main () {
 
     // Unidad de control del arreglo sistolico
     SystolicController controller;
-    controller.n = size; // Tamaño de la matriz
+    controller.n = n; // Tamaño de la matriz
     controller.addr_A = 0; // Direccion de la matriz A
-    controller.addr_B = 16; // Direccion de la matriz B
-    controller.addr_C = 32; // Direccion de la matriz C
+    controller.addr_B = n * n; // Direccion de la matriz B
+    controller.addr_C = n * n * 2; // Direccion de la matriz C
 
       
     controller.new_data = 1; // Indicar que hay nuevos datos para procesar
+    cout << "Cargando matrices A y B en el controlador..." << endl;
 
     // Actualizar el controlador (funcion del clk)
-    for (int i = 0; i < 60; ++i) {
+    for (int i = 0; i < 3000; ++i) {
         // Enviar la info a los PEs
         // Los pesos quedaran asignados en los PEs (assign dentro del controlador)
         for (int i2 = 0; i2 < size; ++i2) {
@@ -77,17 +88,23 @@ int main () {
             }
         }
 
+
         // Memoria
         memoryy.addr = controller.act_addr; // Direccion de memoria
         memoryy.wren = controller.mem_write; // Habilitar escritura si es necesario
         memoryy.data = controller.mem_data_write; // Datos a escribir en memoria
         memoryy.update();
 
+        if (memoryy.wren == 1) {
+            cout << "Escribiendo en memoria en la direccion " << memoryy.addr << ": " << memoryy.data << endl;
+        } else {
+            // cout << "Leyendo de memoria en la direccion " << memoryy.addr << ": " << memoryy.q << endl;
+        }
+
         controller.mem_read = memoryy.q;
         controller.update(); // Actualizar el controlador
-        
 
-        
+
 
         // Enviar los datos de entrada a los PEs (assign)
         for (int j = 0; j < size; ++j) {
@@ -110,13 +127,28 @@ int main () {
         
     }
 
-    for (int i = 0; i < 50; ++i) {
-        if (i % 4 == 0 && i != 0) {
+    for (int i = 0; i < 800; ++i) {
+        if (i % n == 0 && i != 0) {
             cout << endl; // Nueva linea cada 16 elementos
         } 
         cout << memoryy.memory[i] << ", "; // Imprimir la memoria
     }
     cout << endl;
+
+    // Hacer el calculo de A * B sin usar el arreglo sistolico
+    cout << "\nResultado de la multiplicacion A*B (sin arreglo sistolico):" << endl;
+    vector<vector<int>> result(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            int sum = 0;
+            for (int k = 0; k < n; ++k) {
+                sum += matrix_A[i][k] * matrix_B[k][j];
+            }
+            result[i][j] = sum;
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 1;
     
--- reference_model/systoliccontroller.cpp
+++ reference_model/systoliccontroller.cpp
@@ -29,6 +29,7 @@ class SystolicController {
         int matrix_A[4][4];
         int matrix_B[4][4];
         int matrix_C[4][4];
+        int matrix_sum[4][4];
 
         int act_addr;
         int mem_read;
@@ -45,9 +46,11 @@ class SystolicController {
 
         // Agregado para matrices n > 4
         int i, j, k, n_tmp;
+        // int offsetA_i, offsetA_j, offsetB_i, offsetB_j;
 
 
     void update() {
+        cout << "Cycle count: " << cycle_count << ", FSM state: " << static_cast<int>(fsm_state) << endl;
         // Cargar pesos (B) (assign)
         for (int i = 0; i < 4; ++i) {
             for (int j = 0; j < 4; ++j) {
@@ -56,25 +59,52 @@ class SystolicController {
             }
         }
 
+        cout << "i: " << i << ", j: " << j << ", k: " << k << endl;
+
         switch (fsm_state) {
             case State::IDLE: 
                 // Assign
                 cout << "FSM: IDLE" << endl;
                 mem_write = 0;
                 fsm_state = (new_data == 1) ? State::MEMORY : fsm_state;
+
+                // Matrices n > 4
+                n_tmp = n / 4;
+                k, i, j = 0;
+                break;
+            case State::ANALYSIS:
+                // Matrices n > 4
+                
                 break;
             case State::MEMORY:
-                act_addr = addr_A;  // Direccion para obtener matriz A de memoria
+                act_addr = addr_A + (i * 4 * n) + (k * 4);  // Direccion para obtener matriz A de memoria
                 cout << "FSM: MEMORY, " << "act_addr: " << act_addr << endl;
+                cycle_count = 0; // Reiniciar el contador de ciclos
+                mem_write = 0;
                 fsm_state = State::WAITING_MEMORY_A;
                 break;
+            
             case State::WAITING_MEMORY_A:
                 // Esperar obtener datos de memoria
                 // mem_read = memory.read(); -> mem_read
                 cout << "FSM: WAITING_MEMORY_A, " << "act_addr: " << act_addr << ", mem_read: " << mem_read << endl;
-                matrix_A[(act_addr - addr_A) / 4][(act_addr - addr_A) % 4] = mem_read;
-                act_addr = (act_addr < addr_A + 16) ? act_addr + 1 : addr_B;  // Direccion para obtener matriz B de memoria
-                fsm_state = (act_addr >= addr_A + 16) ? State::WAITING_MEMORY_B : State::WAITING_MEMORY_A;
+                matrix_A[cycle_count / 4][cycle_count % 4] = mem_read;
+                
+                
+                // act_addr = (act_addr < addr_A + 16) ? act_addr + 1 : addr_B;  // Direccion para obtener matriz B de memoria
+                // fsm_state = (act_addr >= addr_A + 16) ? State::WAITING_MEMORY_B : State::WAITING_MEMORY_A;
+                // Para n > 4:
+                if (cycle_count >= 15) {
+                    act_addr = addr_B + (k * 4 * n) + (j * 4); // Cambiar a direccion de matriz B
+                    cycle_count = 0; // Reiniciar el contador de ciclos
+                    fsm_state = State::WAITING_MEMORY_B; // Cambiar estado a esperar memoria B
+                } else if ((cycle_count + 1) % 4 == 0) {
+                    act_addr = (act_addr - 3) + n; // Incrementar direccion en 4 para la siguiente fila
+                    cycle_count++;
+                } else {
+                    act_addr++; // Incrementar direccion en 1 para la siguiente columna
+                    cycle_count++;
+                }                
 
                 // IMPRIMIR la matriz A
                 cout << "Matriz A: " << endl;
@@ -89,14 +119,26 @@ class SystolicController {
                 // Alistar para empezar a ejecutar
                 // mem_read = memory.read(); -> mem_read
                 cout << "FSM: WAITING_MEMORY_B, act_addr: " << act_addr << ", mem_read: " << mem_read << endl;
-                matrix_B[(act_addr - addr_B) / 4][(act_addr - addr_B) % 4] = mem_read;
-                act_addr = (act_addr < addr_B + 16) ? act_addr + 1 : addr_C;
+                matrix_B[cycle_count / 4][cycle_count % 4] = mem_read;
+                
                 
                 // data_ready = 1;      // Avisar al control unit para comenzar a operar
-                cycle_count = 0; // Reiniciar el contador de ciclos
 
                 // Asignar la matriz B de lo que sale de memoria
-                fsm_state = (act_addr >= addr_B + 16) ? State::EXECUTE : State::WAITING_MEMORY_B;
+                // fsm_state = (act_addr >= addr_B + 16) ? State::EXECUTE : State::WAITING_MEMORY_B;
+                // act_addr = (act_addr < addr_B + 16) ? act_addr + 1 : addr_C;
+
+                if (cycle_count >= 15) {
+                    act_addr = addr_C + (i * n) + j * 4; // Cambiar a direccion de matriz C
+                    cycle_count = 0; // Reiniciar el contador de ciclos
+                    fsm_state = State::EXECUTE; // Cambiar estado a esperar memoria B
+                } else if ((cycle_count + 1) % 4 == 0) {
+                    act_addr = (act_addr - 3) + n; // Incrementar direccion en 4 para la siguiente fila
+                    cycle_count++;
+                } else {
+                    act_addr++; // Incrementar direccion en 1 para la siguiente columna
+                    cycle_count++;
+                } 
 
                 // IMPRIMIR la matriz B
                 cout << "Matriz B: " << endl;
@@ -114,10 +156,10 @@ class SystolicController {
                 
 
                 // Cargar datos de entrada (A)
-                icol0 = (cycle_count < n) ? cycle_count : 0xFF;
-                icol1 = (cycle_count - 1 < n && cycle_count - 1 >= 0) ? cycle_count - 1 : 0xFF;
-                icol2 = (cycle_count - 2 < n && cycle_count - 2 >= 0) ? cycle_count - 2 : 0xFF;
-                icol3 = (cycle_count - 3 < n && cycle_count - 3 >= 0) ? cycle_count - 3 : 0xFF;
+                icol0 = (cycle_count < 4) ? cycle_count : 0xFF;
+                icol1 = (cycle_count - 1 < 4 && cycle_count - 1 >= 0) ? cycle_count - 1 : 0xFF;
+                icol2 = (cycle_count - 2 < 4 && cycle_count - 2 >= 0) ? cycle_count - 2 : 0xFF;
+                icol3 = (cycle_count - 3 < 4 && cycle_count - 3 >= 0) ? cycle_count - 3 : 0xFF;
 
                 data_up[0] = (icol0 != 0xFF) ? matrix_A[icol0][0] : 0;
                 data_up[1] = (icol1 != 0xFF) ? matrix_A[icol1][1] : 0;
@@ -143,13 +185,29 @@ class SystolicController {
                     cout << endl;
                 }
 
-                if (cycle_count >= n * 2) {
-                    fsm_state = State::WRITEBACK; // Cambiar al estado de escritura
-                    mem_write = 1;
-                    cycle_count = 0; // Reiniciar el contador de ciclos
-                    act_addr = addr_C; // Direccion para escribir la matriz C
-                    mem_data_write = matrix_C[0][0]; // Obtener el dato a escribir (inicialmente el primer elemento de la matriz C)
-                    
+                // Para matrices n > 4
+                if (cycle_count >= 8) {
+                    cout << "Saving results to matrix_sum" << endl;
+                    for (int i = 0; i < 4; ++i) {
+                        for (int j = 0; j < 4; ++j) {
+                            matrix_sum[i][j] += matrix_C[i][j]; // Guardar la matriz C en matrix_sum
+                            cout << matrix_sum[i][j] << " "; // Imprimir el resultado
+                        }
+                        cout << endl;
+                    }
+                    if (k + 1 >= n_tmp) {
+                        fsm_state = State::WRITEBACK; // Cambiar al estado de escritura
+                        mem_write = 1;
+                        cycle_count = 0; // Reiniciar el contador de ciclos
+                        act_addr = addr_C + (i * n * 4) + (j * 4); // Direccion para escribir la matriz C
+                        mem_data_write = matrix_sum[0][0]; // Obtener el dato a escribir (inicialmente el primer elemento de la matriz C)
+                    } else {
+                        k++; // Incrementar k para la siguiente iteracion
+                        // Guardar la matriz C en matrix_sum sumando e imprimirla matriz C
+                        
+                        fsm_state = State::MEMORY; // Volver a memoria para cargar la siguiente parte de la matriz A
+                    }
+
                     // Imprimir la matriz C
                     cout << "Matriz C: " << endl;
                     for (int i = 0; i < 4; ++i) {
@@ -159,11 +217,10 @@ class SystolicController {
                         cout << endl;
                     }
 
+                } else {
+                    // Incrementar el contador de ciclos
+                    cycle_count++;
                 }
-
-                // Incrementar el contador de ciclos
-                cycle_count++;
-
                 break;
             case State::WRITEBACK:
                 cout << "FSM: WRITEBACK" << endl;
@@ -171,18 +228,83 @@ class SystolicController {
                 // Escribir la matriz C en memoria
                 // memory.write();
                 // Pasar a IDLE
-                mem_data_write = matrix_C[cycle_count / 4][cycle_count % 4]; // Obtener el dato a escribir
-                act_addr = addr_C + cycle_count;  // Direccion para obtener matriz B de memoria
-                
-                if (cycle_count >= 16) {
-                    fsm_state = State::IDLE; 
-                    mem_write = 0;
-                }
+                mem_data_write = matrix_sum[(cycle_count + 1) / 4][(cycle_count + 1) % 4]; // Obtener el dato a escribir
+                cout << "Writing to memory at address: " << act_addr << ", data: " << mem_data_write << ", Memory write enabled: " << mem_write << endl;
+                //act_addr = addr_C + cycle_count;  // Direccion para obtener matriz B de memoria
 
-                cycle_count++;
+                // Para matrices n > 4
+                if ((cycle_count + 1) % 4 == 0) {
+                    act_addr = (act_addr - 3) + n; // Incrementar direccion en 4 para la siguiente fila
+                } else {
+                    act_addr++; // Incrementar direccion en 1 para la siguiente columna
+                }
+                
+                // if (cycle_count >= 16 && i + 1 > n_tmp) {
+                //     fsm_state = State::IDLE; // Volver al estado IDLE
+                //     mem_write = 0; // Desactivar escritura en memoria
+                //     // Reiniciar las matrices C y sum
+                //     for (int i = 0; i < 4; ++i) {
+                //         for (int j = 0; j < 4; ++j) {
+                //             matrix_C[i][j] = 0; // Reiniciar matriz C
+                //             matrix_sum[i][j] = 0; // Reiniciar matriz sum
+                //         }
+                //     }
+                // } else if (cycle_count >= 16 && i + 1 < n_tmp) {
+                //     i++; // Incrementar i para la siguiente fila
+                //     j = 0; // Reiniciar j para la siguiente columna
+                //     k = 0; // Reiniciar k para la siguiente iteracion
+                //     fsm_state = State::MEMORY; // Volver a memoria para cargar la siguiente parte de la matriz A
+                //     mem_write = 0; // Desactivar escritura en memoria
+                //     cycle_count = 0; // Reiniciar el contador de ciclos
+                //     // Reiniciar las matrices C y sum
+                //     for (int i = 0; i < 4; ++i) {
+                //         for (int j = 0; j < 4; ++j) {
+                //             matrix_C[i][j] = 0; // Reiniciar matriz C
+                //             matrix_sum[i][j] = 0; // Reiniciar matriz sum
+                //         }
+                //     }
+                if (cycle_count >= 16) {
+                    j++; // Incrementar j para la siguiente columna
+
+                    if (j >= n_tmp) {
+                        j = 0; // Reiniciar j para la siguiente columna
+                        i++; // Incrementar i para la siguiente fila
+                    }
+
+                    if (i >= n_tmp) {
+                        fsm_state = State::IDLE; // Volver al estado IDLE
+                        mem_write = 0; // Desactivar escritura en memoria
+                        // Reiniciar las matrices C y sum
+                        for (int i = 0; i < 4; ++i) {
+                            for (int j = 0; j < 4; ++j) {
+                                matrix_C[i][j] = 0; // Reiniciar matriz C
+                                matrix_sum[i][j] = 0; // Reiniciar matriz sum
+                            }
+                        }
+                    } else { 
+                        k = 0; // Reiniciar k para la siguiente iteracion
+                        cycle_count = 0; // Reiniciar el contador de ciclos
+                        mem_write = 0; // Desactivar escritura en memoria
+                        fsm_state = State::MEMORY; // Volver a memoria para cargar la siguiente parte de la matriz A
+                        // Reiniciar las matrices C y sum
+                        for (int i = 0; i < 4; ++i) {
+                            for (int j = 0; j < 4; ++j) {
+                                matrix_C[i][j] = 0; // Reiniciar matriz C
+                                matrix_sum[i][j] = 0; // Reiniciar matriz sum
+                            }
+                        }
+                    }
+                    
+
+                   
+                } else {
+                    cycle_count++;
+                }
+                
                 break;
 
         }
+        
     }
 
     void load_A(const int src[4][4]) {
